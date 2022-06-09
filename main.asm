8b62936525e180a9325fa928d8893c7a12ae340f
Daniel Svitan <daniel.svitan.team7274dev@gmail.com>
2022-06-09 19:24:19 +0200

    :construction: Adds errors

--- main.asm
+++ main.asm
@@ -11,6 +11,10 @@ section .data
 	askNumMsgLen equ $ - askNumMsg
 	displayNumMsg db "You have entered ", 0x0
 	displayNumMsgLen equ $ - displayNumMsg
+	containsLetterMsg db "Your input contains a letter", 0x10, 0x0
+	containsLetterMsgLen equ $ - containsLetterMsg
+	unexpectedErrorMsg db "Unexpected error occurred", 0x10, 0x0
+	unexpectedErrorMsgLen equ $ - unexpectedErrorMsg
 
 section .bss
 	input resb 8
@@ -43,6 +47,51 @@ _start:
 	mov rdx, 8
 	int 80h
 
+	mov r8, 0
+	mov r9, input
+	inputLoop:
+	mov r10, 0
+
+	numLoop:
+	cmp byte[input+r8], 0x10
+	je exit
+
+	cmp byte[input+r8], 0x30
+	je end
+
+	inc r10
+	cmp r10, 10
+	jne numLoop
+
+	inc r8
+	jmp inputLoop
+
+	end:
+	add r9, r8
+	mov rax, SYS_WRITE
+	mov rbx, STDOUT
+	mov rcx, r9
+	mov rdx, 8
+	int 80h
+	jmp exit
+
+	containsLetter:
+	mov rax, SYS_WRITE
+	mov rbx, STDOUT
+	mov rcx, containsLetterMsg
+	mov rdx, containsLetterMsgLen
+	int 80h
+	jmp exit
+
+	unexpectedError:
+	;mov rax, SYS_WRITE
+	;mov rbx, STDOUT
+	;mov rcx, unexpectedErrorMsg
+	;mov rdx, unexpectedErrorMsgLen
+	;int 80h
+	jmp exit
+
+	exit:
 	mov rax, SYS_EXIT
 	mov rbx, 0
 	int 80h
